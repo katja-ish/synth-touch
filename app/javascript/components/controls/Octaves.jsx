1c68bed54a954559ffd320db7197cd13b8e9946d
semilunar <katyanikitina.me@yandex.ru>
2020-02-04 21:01:28 +0300

    deleted unused components

--- app/javascript/components/controls/Octaves.jsx
+++ app/javascript/components/controls/Octaves.jsx
@@ -1,19 +1,18 @@
-import Tone from 'tone'
-import React from 'react'
+import React from "react";
 
 export default class Octaves extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    let { value } = this.props
+    let { value } = this.props;
 
     this.state = {
       value
-    }
+    };
   }
 
   render() {
-    let { value, min, max, changeOct } = this.props
+    let { value, min, max, changeOct } = this.props;
 
     return (
       <input
@@ -23,6 +22,6 @@ export default class Octaves extends React.Component {
         value={value}
         onChange={changeOct}
       />
-    )
+    );
   }
 }
--- app/javascript/components/synthesizers/KeySynth.jsx
+++ app/javascript/components/synthesizers/KeySynth.jsx
@@ -1,24 +1,21 @@
-import _ from 'lodash'
-import React from 'react'
-import Tone from 'tone'
+import _ from "lodash";
+import React from "react";
+import Tone from "tone";
 
-// import * as effects from '../../tunes/effects'
-// import * as synths from '../../tunes/synths'
-
-import Keyboard from '../controls/Keyboard'
-import Pedalboard from '../effects/Pedalboard'
-import Envelope from '../controls/Envelope'
-import Knob from '../controls/Knob'
-import ToggleView from '../controls/ToggleView'
-import Slider from '../controls/Slider'
+import Keyboard from "../controls/Keyboard";
+import Pedalboard from "../effects/Pedalboard";
+import Envelope from "../controls/Envelope";
+import Knob from "../controls/Knob";
+import ToggleView from "../controls/ToggleView";
+import Slider from "../controls/Slider";
 
 export default class KeySynth extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     let keySynth = new Tone.Synth({
       oscillator: {
-        type: 'sine'
+        type: "sine"
       },
       envelope: {
         attack: 2,
@@ -26,89 +23,89 @@ export default class KeySynth extends React.Component {
         sustain: 0.4,
         release: 1
       }
-    })
+    });
 
-    let gain = new Tone.Gain(0.5)
+    let gain = new Tone.Gain(0.5);
 
-    let defaultWetValue = 0
+    let defaultWetValue = 0;
 
-    keySynth.chain(gain, Tone.Master)
+    keySynth.chain(gain, Tone.Master);
 
     this.state = {
       keySynth,
       gain,
       currentNote: undefined
-    }
+    };
 
     _.bindAll(
       this,
-      'handleMouseDown',
-      'handleMouseUp',
-      'changeWaveType',
-      'toggleEffect',
-      'handleEnvelope',
-      'handleVolume'
-    )
+      "handleMouseDown",
+      "handleMouseUp",
+      "changeWaveType",
+      "toggleEffect",
+      "handleEnvelope",
+      "handleVolume"
+    );
   }
 
   handleMouseDown(note, octave) {
-    let { keySynth, currentNote } = this.state
+    let { keySynth, currentNote } = this.state;
 
-    keySynth.triggerAttack(`${note}${octave}`)
-    currentNote = note
+    keySynth.triggerAttack(`${note}${octave}`);
+    currentNote = note;
 
-    console.log('Down')
+    console.log("Down");
     this.setState({
       keySynth,
       currentNote
-    })
+    });
   }
 
   handleMouseUp() {
-    let { keySynth, currentNote } = this.state
+    let { keySynth, currentNote } = this.state;
 
-    keySynth.triggerRelease()
-    currentNote = undefined
+    keySynth.triggerRelease();
+    currentNote = undefined;
 
-    console.log('Up')
+    console.log("Up");
 
     this.setState({
       keySynth,
       currentNote
-    })
+    });
   }
 
   changeWaveType(value) {
-    let { keySynth } = this.state
-    keySynth.oscillator.type = value
+    let { keySynth } = this.state;
+    keySynth.oscillator.type = value;
 
     this.setState({
       keySynth
-    })
+    });
   }
 
   toggleEffect(effectName) {
-    console.log(effectName)
-    let { effect, wet, on, name } = this.state[effectName]
+    console.log(effectName);
+    let { effect, wet, on, name } = this.state[effectName];
 
-    effect.wet.value = on ? 0 : wet
-    on = !on
+    effect.wet.value = on ? 0 : wet;
+    on = !on;
 
     this.setState({
       [`${effectName}`]: { effect, wet, on, name }
-    })
+    });
   }
 
   handleVolume(a, b, value) {
-    let { gain } = this.state
-    let volume = value
-    console.log('old', gain.gain.value)
-    console.log('comming vol', volume)
-    gain.gain.value = volume
-    console.log('new', gain.gain.value)
+    let { gain } = this.state;
+    let volume = value;
+    console.log("old", gain.gain.value);
+    console.log("comming vol", volume);
+    gain.gain.value = volume;
+    console.log("new", gain.gain.value);
     this.setState({
       gain
-    })
+    });
   }
 
   // handleValueChange(effectName, param, value) {
@@ -136,15 +133,15 @@ export default class KeySynth extends React.Component {
   // }
 
   handleEnvelope(name, param, value) {
-    let synth = this.state[`${name}`]
-    let env = synth.envelope
-    env[param] = value
+    let synth = this.state[`${name}`];
+    let env = synth.envelope;
+    env[param] = value;
 
-    this.forceUpdate()
+    this.forceUpdate();
   }
 
   render() {
-    let { keySynth, currentNote } = this.state
+    let { keySynth, currentNote } = this.state;
     return (
       <div className="keySynthTest">
         <Keyboard
@@ -161,6 +158,6 @@ export default class KeySynth extends React.Component {
           handleVolume={this.handleVolume}
         />
       </div>
-    )
+    );
   }
 }
